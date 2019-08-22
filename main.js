cb002e4e5b9e36571f8ee3e00dc62d13a94e85f5
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-08-23 02:30:45 +0300

    processing tags

--- main.js
+++ main.js
@@ -14,28 +14,40 @@ async function processFile(path){
 
         if (!content) throw "no content";
 
-        const regex = /<("[^"]*"|'[^']*'|[^'">])*>/g;
-        const regexAttirubtes = new RegExp('[\\s\\r\\t\\n]*([a-z0-9\\-_]+)[\\s\\r\\t\\n]*=[\\s\\r\\t\\n]*([\'"])((?:\\\\\\2|(?!\\2).)*)\\2', 'ig');
-
         let found, tags = [], stack = [];
+        const regex = /<("[^"]*"|'[^']*'|[^'">])*>/g;
 
         while (found = regex.exec(content)){
+
+            const regexAttributes = new RegExp('[\\s\\r\\t\\n]*([a-z0-9\\-_]+)[\\s\\r\\t\\n]*=[\\s\\r\\t\\n]*([\'"])((?:\\\\\\2|(?!\\2).)*)\\2', 'ig');
+            const regexId = /<\/?(\w+)(\s+\w+.*?)?\/?>/g;
 
             const tag = found[0].trim(), attributes = {};
 
             let match;
-            while (match = regexAttirubtes.exec(tag) )
+            while (match = regexAttributes.exec(tag) )
                 attributes[match[1]] = match[3];
+
+            let tagName = regexId.exec(tag);
+            if (tagName ) tagName = tagName[1];
+            else tagName = undefined;
 
             tags.push({
                 tag,
+                tagName: tagName ? tagName[1] : '',
                 pos: found.index,
                 attributes,
             });
             regex.lastIndex = found.index+1;
 
-            if (tag[0]==='<' && tag[1]!=='/')
-                stack.push(tag);
+            if (tag[0] === '<' && tag[1] === '/'){
+                if (stack[stack.length-1] !== tagName) throw "invalid closing tags";
+                stack.splice( stack.length-1, 1);
+            }else
+            if (tag[0]==='<' && tag[1]!=='/' && tag.indexOf("/>") !== tag.length-2)
+                stack.push(tagName);
+
+
 
         }
 
